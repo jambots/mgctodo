f1d817d9eb90aa6f0563cf7d5f399ba58809af42
jambots <jambotsdev@gmail.com>
2019-01-19 16:50:09 -0800

    showApi testing

---
+++ controllers/showApiController.js
@@ -0,0 +1,19 @@
+const util = require('./utilController');
+const { MongoClient } = require('mongodb');
+const os = require("os");
+const debug = require('debug')('app:showApiController');
+
+
+exports.showApiTasks = async function (req, res) {
+  try {
+    const dbParams = await util.setupDB();
+    const tasks = await dbParams.collection.find({}).sort({ dueDate: 1 }).toArray();
+    const hostname = os.hostname();
+    res.render('showApiTasks', { tasks, title: 'ToDo List', hostname });
+    dbParams.client.close();
+  }
+
+  catch (err) {
+    debug(err);
+  }
+}
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 const showController = require('../controllers/showController');
+const showApiController = require('../controllers/showApiController');
 const addController = require('../controllers/addcontroller');
 const editController = require('../controllers/editController');
 const deleteController = require('../controllers/deletecontroller');
@@ -15,5 +16,6 @@ router.post('/task/edit/:id', editController.commitEdit);
 router.get('/task/add/', addController.addTask);
 router.post('/task/add/', addController.saveTask);
 router.get('/', showController.showTasks);
+router.get('/api/', showController.showApiTasks);
 
 module.exports = router;
