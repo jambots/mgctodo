b85e81e29fc8bccd4d60044a488148376f9b9106
jambots <jambotsdev@gmail.com>
2019-01-20 17:09:16 -0800

    delete means completed=true

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -7,7 +7,7 @@ const debug = require('debug')('app:booksController');
 exports.showBooks = async function (req, res) {
   try {
     const dbParams = await util.setupDB();
-    const tasks = await dbParams.collection.find({}).sort({ dueDate: 1 }).toArray();
+    const tasks = await dbParams.collection.find({isComplete:'false'}).sort({ dueDate: 1 }).toArray();
     const hostname = os.hostname();
     res.json(tasks);
     dbParams.client.close();
