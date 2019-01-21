537b26599b4ea8bc4c83a01c9daeb24cf232574b
jambots <jambotsdev@gmail.com>
2019-01-21 08:01:50 -0800

    toggle controller

--- controllers/toggleController.js
+++ controllers/toggleController.js
@@ -1,7 +1,19 @@
 const { MongoClient, ObjectId } = require('mongodb');
 const util = require('./utilController');
-const debug = require('debug')('app:completeController');
+const debug = require('debug')('app:toggleController');
 
+exports.toggleSyndication = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const dbParams = await util.setupDB();
+      const task = await dbParams.collection.findOne({ _id: new ObjectId(id) });
+      let status = (task.isUnsyndicated == 'false') ? 'true' : 'false';
+      await dbParams.collection.findOneAndUpdate({ _id: new ObjectId(id) }, { $set: { isUnsyndicated: status } });
+      dbParams.client.close();
+      res.redirect('/');
+  }
+  catch (err) {debug(err);}
+};
 exports.approve = async (req, res) => {
   try {
       const { id } = req.params;
@@ -12,9 +24,7 @@ exports.approve = async (req, res) => {
       dbParams.client.close();
       res.redirect('/');
   }
-  catch (err) {
-      debug(err);
-  }
+  catch (err) {debug(err);}
 };
 exports.unApprove = async (req, res) => {
   try {
@@ -26,7 +36,29 @@ exports.unApprove = async (req, res) => {
       dbParams.client.close();
       res.redirect('/');
   }
-  catch (err) {
-      debug(err);
+  catch (err) {debug(err);}
+};
+exports.ban = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const dbParams = await util.setupDB();
+      const task = await dbParams.collection.findOne({ _id: new ObjectId(id) });
+      let status = 'true'; //(task.isComplete == 'false') ? 'true' : 'false';
+      await dbParams.collection.findOneAndUpdate({ _id: new ObjectId(id) }, { $set: { isBaned: status } });
+      dbParams.client.close();
+      res.redirect('/');
   }
+  catch (err) {debug(err);}
+};
+exports.unBan = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const dbParams = await util.setupDB();
+      const task = await dbParams.collection.findOne({ _id: new ObjectId(id) });
+      let status = 'false'; //(task.isComplete == 'false') ? 'true' : 'false';
+      await dbParams.collection.findOneAndUpdate({ _id: new ObjectId(id) }, { $set: { isBanned: status } });
+      dbParams.client.close();
+      res.redirect('/');
+  }
+  catch (err) {debug(err);}
 };
