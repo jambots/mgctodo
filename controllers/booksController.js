23976e8a89b9648e296ba603e1480f932a1983bf
jambots <jambotsdev@gmail.com>
2019-01-24 10:00:57 -0800

    require hash

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,19 +2,19 @@ const util = require('./utilController');
 const { MongoClient } = require('mongodb');
 const os = require("os");
 const debug = require('debug')('app:booksController');
-const { parse } = require('querystring');
+//const { parse } = require('querystring');
 
 exports.showBooks = async function (req, res) {
+  res.end(req);
+  /*
   let body = '';
   req.on('data', chunk => {
     body += chunk.toString();
   });
   req.on('end', () => {
-    console.log(
-        parse(body)
-    );
     res.json(parse(body));
   });
+  */
 /*
   if(req.hash=="031987ad563836dd8339615bae2abbb3"){
     try {
