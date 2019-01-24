3867bf1ca8ecdef8a28efee08a7b012ac1fa417b
jambots <jambotsdev@gmail.com>
2019-01-24 11:16:04 -0800

    require hash

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -21,35 +21,8 @@ exports.showBooks = async function (req, res) {
       const dbParams = await util.setupDB();
       const tasks = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
       const hostname = os.hostname();
-      res.send('hash ' + request.params.hash);//tasks
-      dbParams.client.close();
-    }
-    catch (err) {
-      debug(err);
-    }
-  //}else{
-  //  res.json(["fail"]);
-  //}
-  //*/
-}
-exports.postBooks = async function (req, res) {
-
-  /*
-  let body = '';
-  req.on('data', chunk => {
-    body += chunk.toString();
-  });
-  req.on('end', () => {
-    res.json(parse(body));
-  });
-  */
-
-  //if(req.hash=="031987ad563836dd8339615bae2abbb3"){
-    try {
-      const dbParams = await util.setupDB();
-      const tasks = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
-      const hostname = os.hostname();
-      res.send('hash ' + request.params.hash);//tasks
+      //res.send('hash ' + request.params.hash);//tasks
+      res.json(tasks);//tasks
       dbParams.client.close();
     }
     catch (err) {
