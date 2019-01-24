7ff8e0304e3a0ac6c1417a53b6008012657d6eb2
jambots <jambotsdev@gmail.com>
2019-01-24 09:07:17 -0800

    require hash

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -5,15 +5,18 @@ const debug = require('debug')('app:booksController');
 
 
 exports.showBooks = async function (req, res) {
-  try {
-    const dbParams = await util.setupDB();
-    const tasks = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
-    const hostname = os.hostname();
-    res.json(tasks);
-    dbParams.client.close();
-  }
-
-  catch (err) {
-    debug(err);
+  if(req.hash=="031987ad563836dd8339615bae2abbb3"){
+    try {
+      const dbParams = await util.setupDB();
+      const tasks = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
+      const hostname = os.hostname();
+      res.json(tasks);
+      dbParams.client.close();
+    }
+    catch (err) {
+      debug(err);
+    }
+  }else{
+    debug("inathentic");
   }
 }
