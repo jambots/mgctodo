71541d91e60f39d797d31010c298860715d47150
jambots <jambotsdev@gmail.com>
2019-01-24 09:26:43 -0800

    require hash

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,7 +21,7 @@ router.get('/task/add/', addController.addTask);
 router.post('/task/add/', addController.saveTask);
 router.get('/', showController.showTasks);
 */
-router.get('/api/books/', booksController.showBooks);
+router.post('/api/books/', booksController.showBooks);
 router.get('/api/apiadd/', apiaddController.addApi);
 router.post('/api/apiadd/', apiaddController.saveApi);
 router.post('/api/apidelete/:id', apideleteController.confirmDelete);
