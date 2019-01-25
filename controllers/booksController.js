dbd4bdb6dc6b0501178f4241b6976584f4c9dc96
jambots <jambotsdev@gmail.com>
2019-01-24 17:51:50 -0800

    require hash

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -35,19 +35,6 @@ exports.showBooks = async function (req, res) {
 exports.postBooks = async function (req, res) {
   //res.write("Response");
     //if(params["hash"]=="031987ad563836dd8339615bae2abbb3"){
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
-    });
-    var params={hash:'', url:''};
-    req.on('end', () => {
-      var parts=body.split('"');
-      for (var p=1;p<parts.length-1; p+=2){
-        var key=parts[p];
-        var val="|"+parts[p+1].split("\r\n")[2]+"|";
-        params[key]=val;
-      }
-   });
 
 
      try {
@@ -55,6 +42,19 @@ exports.postBooks = async function (req, res) {
        const tasks = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
        const hostname = os.hostname();
        //res.send('hash ' + request.params.hash);//tasks
+       let body = '';
+       req.on('data', chunk => {
+           body += chunk.toString();
+       });
+       var params={hash:'', url:''};
+       req.on('end', () => {
+         var parts=body.split('"');
+         for (var p=1;p<parts.length-1; p+=2){
+           var key=parts[p];
+           var val="|"+parts[p+1].split("\r\n")[2]+"|";
+           params[key]=val;
+         }
+      });
        if(params["hash"]=="031987ad563836dd8339615bae2abbb3"){
          res.json(tasks);//tasks
        }
