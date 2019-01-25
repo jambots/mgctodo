b447766f387bc84cbc5d44c15ac4d7cc3752bd06
jambots <jambotsdev@gmail.com>
2019-01-24 18:28:16 -0800

    require hash

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -41,20 +41,23 @@ exports.showBooks = async function (req, res) {
 exports.postBooks = async function (req, res) {
   //res.write("Response");
     //if(params["hash"]=="031987ad563836dd8339615bae2abbb3"){
-    var params={hash:"", url:""};
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
-    });
-    req.on('end', () => {
-      var parts=body.split('"');
-      for (var p=1;p<parts.length-1; p+=2){
-        var key=parts[p];
-        var val="|"+parts[p+1].split("\r\n")[2]+"|";
-        params[key]=val;
+    try {
+      const dbParams = await util.setupDB();
+      const tasks = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
+      const hostname = os.hostname();
+      //res.send('hash ' + request.params.hash);//tasks
+      //if(params["hash"]=="031987ad563836dd8339615bae2abbb3"){
+        res.json(tasks);//tasks
+      //}
+      else{
+        res.json(['no hash']);
       }
-      res.json(params);
-   });
+      dbParams.client.close();
+    }
+    catch (err) {
+      debug(err);
+    }
+
 
 /*
      try {
