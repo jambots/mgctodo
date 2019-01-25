5c97ccfce026858fc35ba68c4222a2e9cdf2b1f0
jambots <jambotsdev@gmail.com>
2019-01-25 03:53:43 -0800

    require hash

--- controllers/apiListController.js
+++ controllers/apiListController.js
@@ -10,33 +10,37 @@ exports.listSites = async function (req, res) {
     //if(params["hash"]=="031987ad563836dd8339615bae2abbb3"){
     try {
       const dbParams = await util.setupDB();
+      const sites = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
       const hostname = os.hostname();
+      const params = await myfunction(req);
+      if(params.hash=="031987ad563836dd8339615bae2abbb3"){
+        res.json(sites);
+      }
+      else{
+        res.json(params);
+      }
+
       //res.send('hash ' + request.params.hash);//tasks
       //if(params["hash"]=="031987ad563836dd8339615bae2abbb3"){
-      var params={hash:"", url:""};
-      let body = '';
-      req.on('data', chunk => {
-          body += chunk.toString();
-      });
-      req.on('end', () => {
-        var parts=body.split('"');
-        for (var p=1;p<parts.length-1; p+=2){
-          var key=parts[p];
-          var val=parts[p+1].split("\r\n")[2];
-          params[key]=val;
-        }
-        if(params.hash=="031987ad563836dd8339615bae2abbb3"){
-          const tasks = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
-          res.json(tasks);
-        }
-        else{
-          res.json(params);
-        }
-     });
       dbParams.client.close();
     }
     catch (err) {
       debug(err);
     }
-
+}
+const myFunction=asynch function(req){
+  var params={hash:"", url:""};
+  let body = '';
+  req.on('data', chunk => {
+      body += chunk.toString();
+  });
+  req.on('end', () => {
+    var parts=body.split('"');
+    for (var p=1;p<parts.length-1; p+=2){
+      var key=parts[p];
+      var val=parts[p+1].split("\r\n")[2];
+      params[key]=val;
+    }
+  });
+  return params;  
 }
