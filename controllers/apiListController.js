df3a256a0287f095f9c9fd1e546b21034086782a
jambots <jambotsdev@gmail.com>
2019-01-25 16:21:30 -0800

    require hash

--- controllers/apiListController.js
+++ controllers/apiListController.js
@@ -11,10 +11,11 @@ exports.listSites = async function(req, res) {
     const dbParams = await util.setupDB();
     const sites = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
     const hostname = os.hostname();
-    req.body.amazon=new Date().getTime();
+    const serverTime=Math.floor(new Date().getTime()/1000);
+    const deltaSec=Math.abs=Number(req.body.time-serverTime);
     //if(req.body.hash=="031987ad563836dd8339615bae2abbb3"){
     if(req.headers.authorization=="031987ad563836dd8339615bae2abbb3"){
-      res.json(req.body);//sites
+      res.json(deltaSec);//sites
     }
     else{
       res.json(req.body);
