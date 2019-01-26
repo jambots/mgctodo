038356c821d0a19d42d929fb3f209b3b3d1d1a8e
jambots <jambotsdev@gmail.com>
2019-01-26 11:21:38 -0800

    require hash

--- controllers/apiUnsyndicateController.js
+++ controllers/apiUnsyndicateController.js
@@ -14,11 +14,12 @@ exports.unsyndicateSite = async function(req, res) {
     if(returnObj.auth.auth==true){
       //const sites = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
       //returnObj.records=sites;
+/*
       const task = await dbParams.collection.findOne({ _id: new ObjectId(siteId) });
       //let status = (task.isComplete == 'false') ? 'true' : 'false';
       let status = 'true';
       await dbParams.collection.findOneAndUpdate({ _id: new ObjectId(siteId) }, { $set: { isUnsyndicated: status } });
-
+*/
     }
     res.json(returnObj);
     dbParams.client.close();
