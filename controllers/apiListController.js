ea6bdb454dac6f58d3036c9772250348f205a479
jambots <jambotsdev@gmail.com>
2019-01-25 16:03:22 -0800

    require hash

--- controllers/apiListController.js
+++ controllers/apiListController.js
@@ -12,7 +12,9 @@ exports.listSites = async function(req, res) {
     const sites = await dbParams.collection.find({isUnsyndicated:'false', isBanned:'false'}).sort({ dueDate: 1 }).toArray();
     const hostname = os.hostname();
     if(req.body.hash=="031987ad563836dd8339615bae2abbb3"){
-      res.json(sites);
+      res.write(JSON.stringify(req.headers));
+      
+      //res.json(sites);
     }
     else{
       res.json(req.body.hash);
